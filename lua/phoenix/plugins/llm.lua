531c7f53c30a1adb2c88e88abe7cb8172506395e
Batuhan Ergun <ergunbatuhan@icloud.com>
2024-07-25 02:07:40 +0300

    unused keymaps

--- lua/phoenix/plugins/llm.lua
+++ lua/phoenix/plugins/llm.lua
@@ -3,9 +3,8 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local system_prompt =
-    'You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks'
-    local helpful_prompt =
-    'You are a helpful assistant. What I have sent are my notes so far. You are very curt, yet helpful.'
+      'You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks'
+    local helpful_prompt = 'You are a helpful assistant. What I have sent are my notes so far. You are very curt, yet helpful.'
     local dingllm = require 'dingllm'
 
     local function anthropic_help()
@@ -28,10 +27,6 @@ return {
       }, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
     end
 
-    vim.keymap.set({ 'n', 'v' }, '<leader>k', groq_replace, { desc = 'llm groq' })
-    vim.keymap.set({ 'n', 'v' }, '<leader>K', groq_help, { desc = 'llm groq_help' })
-    vim.keymap.set({ 'n', 'v' }, '<leader>L', openai_help, { desc = 'llm openai_help' })
-    vim.keymap.set({ 'n', 'v' }, '<leader>l', openai_replace, { desc = 'llm openai' })
     vim.keymap.set({ 'n', 'v' }, '<leader>I', anthropic_help, { desc = 'llm anthropic_help' })
     vim.keymap.set({ 'n', 'v' }, '<leader>i', anthropic_replace, { desc = 'llm anthropic' })
   end,
