289be8c8c962249698870801f37fe3ce33ef486e
Batuhan Ergun <ergunbatuhan@icloud.com>
2024-07-29 01:40:16 +0300

    rust lang support

---
+++ lua/phoenix/plugins/rust.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'rust-lang/rust.vim',
+    config = function()
+      -- Optional: Add any specific configurations for rust.vim here
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+}
